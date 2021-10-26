08522436d080a7e6920c84882fc253369366f0c9
amarcel <amarcel.pro@gmail.com>
2021-10-26 10:47:48 +0200

    new url

--- api/urls.py
+++ api/urls.py
@@ -3,13 +3,8 @@ from django.urls import include, path
 from rest_framework import routers
 from .views import Parrains, CreateParrains
 
-# router = routers.DefaultRouter()
-# router.register(r'parrains', views.ParrainViewSet)
-
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    # path('', include(router.urls)),
+    path('parrains/', Parrains.as_view()),
     path('parrains/<str:campaignToken>/<str:userCode>', Parrains.as_view()),
-    path('create_parrains', CreateParrains.as_view())
+    path('create_parrains/', CreateParrains.as_view())
 ]
