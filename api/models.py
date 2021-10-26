9d3f84c9fa1891b13d2586720d989f6bde3c889f
amarcel <amarcel.pro@gmail.com>
2021-10-26 12:20:38 +0200

    try

--- api/models.py
+++ api/models.py
@@ -14,7 +14,6 @@ class Campaign(models.Model):
     background_color = models.CharField(max_length=60)
     text_color = models.CharField(max_length=60)
     message = models.CharField(max_length=200)
-    web_site = models.CharField(max_length=200)
 
     def message_personnalized(self, parrain):
         return self.message.replace("{{firstName}}", parrain.firstName)
